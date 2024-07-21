467febbed893b0f6811249fed6cfa5ec6948e97c
AbdullahOmar97 <AbdullahOmar@outlook.com>
2024-07-21 08:54:01 +0300

    add list of book and a nav to go to the home page.
    add Details Book page to represent the details for a single book.

--- book/urls.py
+++ book/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView, BookListView
+from .views import HomePageView, BookListView, book_detail
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('books/', BookListView.as_view(), name='book_list'),
+    path('book/<int:book_id>/', book_detail, name='book_detail'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, TemplateView
 from .models import Book
 
@@ -11,3 +11,7 @@ class BookListView(ListView):
     model = Book
     template_name = 'list_of_books.html'
     context_object_name = 'books'
+
+def book_detail(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'book_detail.html', {'book': book})
